3689effe95c0e025f0cefd0c0e01a1d85c6ac8a7
Kenichiro-Ogawa <k.ogawa.1021@gmail.com>
2020-07-23 12:02:52 +0900

    make index

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_23_021032) do
+ActiveRecord::Schema.define(version: 2020_07_23_023947) do
 
-  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "image", null: false
     t.string "name", null: false
     t.string "info", null: false
