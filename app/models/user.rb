e3e3a23f7756a9d320a53cca0890d5d0f9bbc915
Kenichiro-Ogawa <k.ogawa.1021@gmail.com>
2020-07-23 19:14:00 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :items
+
+  validates :name, presence: true, uniqueness: {case_sensitive: true}
+  validates :email, presence: true, uniqueness: {case_sensitive: true}
+  validates :family_name, presence: true
+  validates :first_name, presence: true
+  validates :family_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birth_year, presence: true
+  validates :birth_month, presence: true
+  validates :birth_date, presence: true
 end
