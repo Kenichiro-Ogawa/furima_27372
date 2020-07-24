248ccac94b5c5f22ebb87d71cb4349af915277bc
Kenichiro-Ogawa <k.ogawa.1021@gmail.com>
2020-07-24 16:32:26 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,10 @@ class User < ApplicationRecord
 
   has_many :items
 
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])[a-z\d]\z/
   validates :name, presence: true, uniqueness: {case_sensitive: true}
   validates :email, presence: true, uniqueness: {case_sensitive: true}
+  validates :password, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }
   validates :family_name, presence: true
   validates :first_name, presence: true
   validates :family_name_kana, presence: true
