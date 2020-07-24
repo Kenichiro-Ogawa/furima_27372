f09299ad178c32da2609c6880ad49a0de6e8dae9
Kenichiro-Ogawa <k.ogawa.1021@gmail.com>
2020-07-24 18:52:06 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,15 @@ class User < ApplicationRecord
   VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
   VALID_KANA_REGEX = /\A([ァ-ン]|ー)\z/
 
-  validates :nickname, presence: true, uniqueness: {case_sensitive: true}
-  validates :email, presence: true, uniqueness: {case_sensitive: true}
-  validates :password, presence: true, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }
-  validates :family_name, presence: true, format: { with: VALID_NAME_REGEX }
-  validates :first_name, presence: true, format: { with: VALID_NAME_REGEX }
-  validates :family_name_kana, presence: true, format: { with: VALID_KANA_REGEX }
-  validates :first_name_kana, presence: true, format: { with: VALID_KANA_REGEX }
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :nickname, uniqueness: {case_sensitive: true}
+    validates :email, uniqueness: {case_sensitive: true}
+    validates :password, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }
+    validates :family_name, format: { with: VALID_NAME_REGEX }
+    validates :first_name, format: { with: VALID_NAME_REGEX }
+    validates :family_name_kana, format: { with: VALID_KANA_REGEX }
+    validates :first_name_kana, format: { with: VALID_KANA_REGEX }
+    validates :birthday, presence: true
+  end
+
 end
