0cbd725f3646ce4318c1bb060102f3638b8ab02d
Kenichiro-Ogawa <k.ogawa.1021@gmail.com>
2020-07-25 16:49:36 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,14 @@
 class Item < ApplicationRecord
   belongs_to :user
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :delivery_fee
+  belongs_to_active_hash :delivery_time
+  belongs_to_active_hash :prefecture
+
+  validates :image, :name, :info, :category, :status, :delivery_fee, :prefecture, :delivery_time, :price, presence: true
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id, :status_id, :delivery_fee_id, :delivery_time_id, :prefecture_id
+  end
 end
