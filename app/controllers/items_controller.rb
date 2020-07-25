9ab42fa875b6660187dc3b09a5b3f76db44b59bd
Kenichiro-Ogawa <k.ogawa.1021@gmail.com>
2020-07-25 17:10:21 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,39 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: :index
   def index
     @items = Item.all.order('created_at DESC')
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :image,
+      :name,
+      :info,
+      :category,
+      :status,
+      :delivery_fee,
+      :prefecture,
+      :delivery_time,
+      :price
+    ).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
+
 end
