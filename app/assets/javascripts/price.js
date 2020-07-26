841f8a83cd9e572eb3b77fb06d71bdb049bdb685
Kenichiro-Ogawa <k.ogawa.1021@gmail.com>
2020-07-26 22:24:06 +0900

    Update price.js

--- app/assets/javascripts/price.js
+++ app/assets/javascripts/price.js
@@ -1,5 +1,7 @@
 $(function () {
   $(".price-input").on("change", function () {
+    $("#add-tax-price").empty();
+    $("#profit").empty();
     let fee = $(".price-input").val() * 0.1;
     let profit = $(".price-input").val() - fee;
     $("#add-tax-price").append(fee);
