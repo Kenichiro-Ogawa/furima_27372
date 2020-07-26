af4916c492c404c5d94aedcecacaf2608a60fc8f
Kenichiro-Ogawa <k.ogawa.1021@gmail.com>
2020-07-26 20:01:42 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   has_one_attached :image
-  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :status
   belongs_to_active_hash :delivery_fee
