2bf6943303651303d54adb1fe367658d3edc8986
Kenichiro-Ogawa <k.ogawa.1021@gmail.com>
2020-07-26 19:08:47 +0900

    edit factories/item.rb, item_spec.rb

--- spec/factories/item.rb
+++ spec/factories/item.rb
@@ -9,6 +9,7 @@ FactoryBot.define do
     delivery_fee_id {2}
     prefecture_id {2}
     delivery_time_id {2}
+    price {1000}
   end
 
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
-describe User do
+
+describe Item do
   describe '#create' do
-    it "" do
+    it "全ての値が正しく入力されていれば保存できること" do
+      item = build(:item)
+      binding.pry
+      expect(item).to be_valid
     end
   end
 end
