f797381deb57111b5ba9f307fa595f150b558793
Kenichiro-Ogawa <k.ogawa.1021@gmail.com>
2020-07-27 14:18:27 +0900

    Update item.rb

--- spec/factories/item.rb
+++ spec/factories/item.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
 
   factory :item do
-    image {File.open("#{Rails.root}/public/images/test_image.jpg")}
     name  {'test'}
     info  {'test_info'}
     category_id {2}
@@ -10,6 +9,7 @@ FactoryBot.define do
     prefecture_id {2}
     delivery_time_id {2}
     price {1000}
+    user
   end
 
 end
