71cab5c7c9adfc6c4643d795430c163f4299f491
Kenichiro-Ogawa <k.ogawa.1021@gmail.com>
2020-07-28 17:02:44 +0900

    use authenticate_user!

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,4 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -37,7 +36,4 @@ class ItemsController < ApplicationController
     ).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    redirect_to action: :index unless user_signed_in?
-  end
 end
