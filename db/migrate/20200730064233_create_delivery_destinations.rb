d7b14381272b7c3639ad87f6cf9a59ecf5abf03d
Kenichiro-Ogawa <k.ogawa.1021@gmail.com>
2020-07-30 16:05:15 +0900

    edit migration

--- db/migrate/20200730064233_create_delivery_destinations.rb
+++ db/migrate/20200730064233_create_delivery_destinations.rb
@@ -1,7 +1,13 @@
 class CreateDeliveryDestinations < ActiveRecord::Migration[6.0]
   def change
     create_table :delivery_destinations do |t|
-
+      t.references :purchase, null: false, foreign_key: true
+      t.integer :postal_code, null: false
+      t.string :prefecture_id,  null: false
+      t.string :city, null: false
+      t.string :street, null: false
+      t.string :building
+      t.integer :phone, null: false
       t.timestamps
     end
   end
