5ba8d64a93ce5044f49f94bcc0ccf6dc3705253a
Kenichiro-Ogawa <k.ogawa.1021@gmail.com>
2020-07-31 16:20:29 +0900

    Create purchase_delivery_destination.rb

---
+++ app/forms/purchase_delivery_destination.rb
@@ -0,0 +1,22 @@
+class PurchaseDeliveryDestination
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :street, :building, :phone
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+
+  validates :prefecture_id, numericality: { other_than: 1 }
+
+  def save
+    purchase = Purchase.create(user_id: current_user.id, item_id: @item.id)
+    DeliveryDestination.create( purchase_id: purchase.id,
+                                postal_code: postal_code,
+                                prefecture_id: prefecture_id,
+                                city: city,
+                                street: street,
+                                building: building,
+                                phone: phone,
+                              )
+  end
+
+end
