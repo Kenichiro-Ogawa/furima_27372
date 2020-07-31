c0430ce221187b99ba3c19925d6cff59124184cd
Kenichiro-Ogawa <k.ogawa.1021@gmail.com>
2020-07-31 16:47:01 +0900

    Update purchase_delivery_destination.rb

--- app/forms/purchase_delivery_destination.rb
+++ app/forms/purchase_delivery_destination.rb
@@ -2,9 +2,6 @@ class PurchaseDeliveryDestination
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :street, :building, :phone
 
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :prefecture
-
   validates :prefecture_id, numericality: { other_than: 1 }
 
   def save
