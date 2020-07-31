3378c31c78733a3b7a8592c2daf6384ec4b041bd
Kenichiro-Ogawa <k.ogawa.1021@gmail.com>
2020-07-31 21:44:35 +0900

    move card.js

--- app/assets/javascripts/card.js
+++ app/assets/javascripts/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
+  Payjp.setPublicKey("pk_test_e480753b3c1b7b65a885f1ad");
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -20,7 +20,7 @@ const pay = () => {
         const renderDom = document.getElementById("charge-form");
         const tokenObj = `<input value=${token} type="hidden" name='token'>`;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
-
+        console.log(tokenObj);
         document.getElementById("number").removeAttribute("name");
         document.getElementById("cvc").removeAttribute("name");
         document.getElementById("exp_month").removeAttribute("name");
