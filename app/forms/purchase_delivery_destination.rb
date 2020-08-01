f42d559b76bfc8ac200337b372acdd3357184db7
Kenichiro-Ogawa <k.ogawa.1021@gmail.com>
2020-08-01 17:13:13 +0900

    Update purchase_delivery_destination.rb

--- app/forms/purchase_delivery_destination.rb
+++ app/forms/purchase_delivery_destination.rb
@@ -1,11 +1,11 @@
 class PurchaseDeliveryDestination
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :street, :building, :phone
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :street, :building, :phone
 
   validates :prefecture_id, numericality: { other_than: 1 }
 
   def save
-    purchase = Purchase.create(user_id: current_user.id, item_id: @item.id)
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
     DeliveryDestination.create( purchase_id: purchase.id,
                                 postal_code: postal_code,
                                 prefecture_id: prefecture_id,
