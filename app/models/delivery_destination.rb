4a7eea51eb398585c271656c729b9f43ebcdaf33
Kenichiro-Ogawa <k.ogawa.1021@gmail.com>
2020-08-01 21:28:22 +0900

    Update delivery_destination.rb

--- app/models/delivery_destination.rb
+++ app/models/delivery_destination.rb
@@ -4,5 +4,4 @@ class DeliveryDestination < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
 
-  validates :prefecture_id, numericality: { other_than: 1 }
 end
